4fa7d1fa346f6d218ce53d736e5ad4af78d25bc6
taoshidev1 <myasavolian90@gmail.com>
2024-02-27 12:42:18 -0800

    adding valiweights

--- serve.py
+++ serve.py
@@ -1,33 +1,50 @@
 from flask import Flask, jsonify
 import os
-import sys
 
 app = Flask(__name__)
 
+
 # Endpoint to read and serve JSON data from cmw.json
-@app.route('/cmw', methods=['GET'])
+@app.route("/cmw", methods=["GET"])
 def get_cmw_data():
-    cmw_json_path = os.path.abspath(os.path.join(path, 'cmw.json'))
-    if os.path.exists(cmw_json_path):
-        with open(cmw_json_path, 'r') as file:
-            data = file.read()
-        return jsonify(data)
-    else:
-        return "cmw.json not found", 404
+	cmw_json_path = os.path.abspath(os.path.join(path, "outputs/cmw.json"))
+	if os.path.exists(cmw_json_path):
+		with open(cmw_json_path, "r") as file:
+			data = file.read()
+		return jsonify(data)
+	else:
+		return f"{cmw_json_path} not found", 404
+
 
 # Endpoint to read and serve JSON data from latest_predictions.json
-@app.route('/predictions', methods=['GET'])
+@app.route("/predictions", methods=["GET"])
 def get_predictions_data():
-    predictions_json_path = os.path.abspath(os.path.join(path, 'latest_predictions.json'))
-    if os.path.exists(predictions_json_path):
-        with open(predictions_json_path, 'r') as file:
-            data = file.read()
-        return jsonify(data)
-    else:
-        return "latest_predictions.json not found", 404
+	predictions_json_path = os.path.abspath(
+		os.path.join(path, "outputs/latest_predictions.json")
+	)
+	if os.path.exists(predictions_json_path):
+		with open(predictions_json_path, "r") as file:
+			data = file.read()
+		return jsonify(data)
+	else:
+		return f"{predictions_json_path} not found", 404
 
 
-if __name__ == '__main__':
+@app.route("/weights", methods=["GET"])
+def get_predictions_data():
+	predictions_json_path = os.path.abspath(
+		os.path.join(path, "weights/valiweights.json")
+	)
+	if os.path.exists(predictions_json_path):
+		with open(predictions_json_path, "r") as file:
+			data = file.read()
+		return jsonify(data)
+	else:
+		return f"{predictions_json_path} not found", 404
 
-    path = "~/time-series-prediction-subnet/validation/outputs/"
-    app.run(host='0.0.0.0', port=80)
+
+if __name__ == "__main__":
+	from waitress import serve
+
+	path = "time-series-prediction-subnet/validation/"
+	serve(app, host="0.0.0.0", port=48888)
